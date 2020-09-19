242fb0aec6a2586a39472bba71917596111483ad
Ayumi Haruna <ayumi.harun@gmail.com>
2020-09-18 21:16:19 -0500

    add responsive design and banner carousel for index.html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
+//when page end loading
+document.addEventListener("DOMContentLoaded", function(event) {
+    //set diferent "infancias menu" functions to desktop or mobile
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+        //on click, resize button, and then redirect
+        ageBtns.forEach( btn => {
+            btn.addEventListener('click', e => {
+                resizeBtn(btn);
+                setTimeout(()=>{
+                    window.location = btn.dataset.link;
+                },1000);
+            });
+        });
+    } else {
+        //on hover resize button, on click redirect
+        ageBtns.forEach( btn => {
+            btn.addEventListener('mouseenter', e => {
+                resizeBtn(btn);
+            });
+            btn.addEventListener('click', e => {
+                window.location = btn.dataset.link;
+            });
+        });
+    }
+
+    //initialize the swiper carousel
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true,
+        },
+    });
+});
+
 // create an array with age buttons
 const ageBtns = Array.prototype.slice.call(
-    document.getElementsByClassName("ageBtn")
+    document.getElementsByClassName("ageBtn"),
 );
 
 // resize buttons on click
