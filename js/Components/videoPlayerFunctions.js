f3bc931878bf51dbe577e6e6c6a2d107787ec08e
Ayumi Haruna <ayumi.harun@gmail.com>
2020-10-16 20:17:38 -0500

    reparé el selector de edades en el index, ya ocupa todo el ancho de pantalla, añadí recomendaciones de video en infancia1 y infancia2

--- js/Components/videoPlayerFunctions.js
+++ js/Components/videoPlayerFunctions.js
@@ -1,3 +1,156 @@
+//draw video component for infancia1, infancia2
+function drawComponentVideos(age){  
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    let toDraw = ``;
+
+    toDraw += `
+        <div class="row comp_videos age_`+age+`">
+            <div class="col-lg-8 player_container">
+                <div>
+                    <iframe id="true_video_player" src="" frameborder="0" allowfullscreen></iframe>
+                </div>
+
+                <div class="video_info" id="video_info">
+
+                </div>
+            </div>
+
+            <div class="col-lg-4 video_list_container">
+    `;
+
+    //add all playlists (data from video_player_data.js)
+    if( Object.keys(video_player_data).length > 0){
+        for (let i = 0; i < Object.keys(video_player_data).length; i++) {
+            let current_key = Object.keys(video_player_data)[i];
+
+            if( video_player_data[current_key]["age"] == 0 ||
+                video_player_data[current_key]["age"] == age )
+            {
+                toDraw += `
+                    <div class="row">
+                        <div class="col-12 playList_button" data-target="video_list_`+current_key+`" onClick="show_play_list(this)">
+                            `+ video_player_data[current_key]["name"] +`
+                        </div>
+                        <div class="col-12 playList_container" id="video_list_`+current_key+`">
+                `;
+
+                //add all video types from this playList
+                if( Object.keys(video_player_data[current_key]["type"]).length > 0){
+                    for (let j = 0; j < Object.keys(video_player_data[current_key]["type"]).length; j++) {
+                        let current_type = Object.keys(video_player_data[current_key]["type"])[j];
+
+                        toDraw += `
+                            <div class="row">
+                                <div class="col-12 typeList_button" data-target="type_list`+current_key+`-`+current_type+`" onClick="show_type_list(this)">
+                                `+ video_player_data[current_key]["type"][current_type]["name"] +`
+                                    <div class="vid_count">`
+                                        +Object.keys(video_player_data[current_key]["type"][current_type]["playList"]).length+` videos
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 typeList_container" id="type_list`+current_key+`-`+current_type+`">
+                        `;
+                            
+                        //add all tracks from this playList (descendant mode)
+                        if( Object.keys(video_player_data[current_key]["type"][current_type]["playList"]).length > 0){
+                            for (let k = Object.keys(video_player_data[current_key]["type"][current_type]["playList"]).length - 1; k >= 0; k--) {
+                                let current_vid = Object.keys(video_player_data[current_key]["type"][current_type]["playList"])[k];
+                                
+                                toDraw += `
+                                    <div class="track_button" 
+                                        data-list="`+current_key+`" 
+                                        data-type="`+current_type+`"
+                                        data-track="`+current_vid+`"
+                                        onClick="change_video(this)">`+
+                                        current_vid+`. `+video_player_data[current_key]["type"][current_type]["playList"][current_vid]["name"]
+                                    +`</div>
+                                `;
+                            }
+                        }
+
+                        toDraw += `
+                            </div>
+                        `;
+                    }
+                }
+            
+
+                toDraw += `
+                        </div>
+                    </div>
+                `;
+            }
+        }
+    } 
+
+
+    //draw recent recomendations (only 3 of them)
+    var videoCollection = getRecentVideos();
+                
+    toDraw += `
+            </div>
+
+            <div class="col-12 vid_list">
+                <div class="row">
+    `;
+
+    // muestra videos desde current show index hasta 6 videos siguientes
+    for (let i = 0; i < 3; i++) {
+        if( videoCollection[i] !== undefined ){ // if videocollection exists print
+            let key = videoCollection[i]["key"];
+            let type_key = videoCollection[i]["type_key"];
+            let list_key = videoCollection[i]["list_key"];
+            
+            toDraw += `
+                    <div 
+                        class="col-sm-4 vid_card" 
+                        data-list="`+key+`" 
+                        data-type="`+type_key+`"
+                        data-track="`+list_key+`"
+                        onClick="change_video(this)"
+                    >
+                        <div class="img_container">
+                            <img 
+                                src="https://i.ytimg.com/vi/`+ (  (video_player_data[key]["type"][type_key]["playList"][list_key]["url"]).replace( 'https://www.youtube.com/embed/', '')  ) +`/1.jpg"
+                                alt=" video de ${video_player_data[key]["type"][type_key]["playList"][list_key]["name"]}"
+                            />
+                        </div>
+                        <div class="info_container">
+                            <div class="title">
+                                ${video_player_data[key]["type"][type_key]["playList"][list_key]["name"]}
+                            </div>
+                            <div class="description"> 
+                                `+
+                                    ( (video_player_data[key]["type"][type_key]["playList"][list_key]["description"]).substring(0,150) )
+                                +
+                                    (( (video_player_data[key]["type"][type_key]["playList"][list_key]["description"]).length > 150 )? "..." : "" )
+                                +`
+                            </div> 
+                        </div> 
+                    </div>
+            `;                      
+        } else {    //else stop for and hide button
+            //hide button
+            break;
+        }
+    }
+
+
+    toDraw += `
+            </div>
+        </div>
+    `;
+
+    document.getElementById("component_container").innerHTML = toDraw;
+
+    //auto select first play_list
+    document.getElementsByClassName("playList_button")[0].click();
+    //auto select first type_list
+    document.getElementsByClassName("typeList_button")[0].click();
+    //auto select first track
+    document.getElementsByClassName("track_button")[0].click();
+}
+
 // show selected playList
 function show_play_list( elm ){
     //loop all playList fields
@@ -63,12 +216,81 @@ function change_video( elm ){
 }
 
 
+function getRecentVideos(){
+    var videoCollection = new Array();
+    // get an array of avalible videos for this age
+    if( Object.keys(video_player_data).length > 0 ){
+        for (let i = 0; i < Object.keys(video_player_data).length; i++) {   //recorre cada lista
+            let key = Object.keys(video_player_data)[i];
+            
+            if( video_player_data[key]["age"] == 0 || video_player_data[key]["age"] == age ){
+                for(let j = 0; j < Object.keys(video_player_data[key]["type"]).length; j++) {     //recorre cada type
+                    let type_key = Object.keys(video_player_data[key]["type"])[j]
+
+                    for (let k = 0; k < Object.keys(video_player_data[key]["type"][type_key]["playList"]).length; k++) {    //recorre el playlist
+                        let list_key = Object.keys(video_player_data[key]["type"][type_key]["playList"])[k];     
+
+                        videoCollection.push({
+                            "key": key, 
+                            "type_key": type_key, 
+                            "list_key": list_key,
+                            "date" : video_player_data[key]["type"][type_key]["playList"][list_key]["date"]
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    // order the array by dates
+    videoCollection.sort((a, b) => {
+        let c = new Date(a.date);
+        let d = new Date(b.date);
+        
+        return d - c;
+    });
+
+    return videoCollection;
+}
+
 //  ---------------------------------------
 //      functions for video player 2
 //  ---------------------------------------
+//draw video component for jovenes
+function drawComponentVideos2(age){
+    var toDraw = `
+        <div class="row">
+            <div class="col-12 vid_2">
+                <div class="row" id="vid_listContainer">
+
+                </div>
+            </div>
+
+            <div class="col-12 vidShowContainer">
+                <button id="vidShowBtn" onClick="print_videoList2()">Mostras mas</button>
+            </div>
+        </div>
+
+        <div class="vid_player2_screen" id="vid_player2_screen">
+            <div class="vid_player2_container">
+                <button class="close_player" onClick="closeVid2()">
+                    <i class="far fa-times-circle"></i>
+                </button>
+                <iframe id="true_video_player" src="" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>  
+    `;
+    
+    document.getElementById("component_container").innerHTML = toDraw;
+
+    // print video list     (function from videoPlayerFunctions.js)
+    print_videoList2();
+}
 
 // print video list on jovenes, formadores
+currentShowIndex = 1;               //for show video list index
 function print_videoList2(){
+    var videoCollection = getRecentVideos();
     // print list with ordered array
     let toDraw = `
         <div class="row">
